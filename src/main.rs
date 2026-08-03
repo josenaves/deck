96f7d735d4fccbe6c6c4ca118307edd59f44b222
José Naves <josenaves@gmail.com>
2026-08-03 17:55:01 -0300

    feat: add inherent impl

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        // List of suits - hearts, diamonds, clubs, spades
+        let suits = ["hearts", "diamonds", "clubs", "spades"];
+
+        // List of values - 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A 
+        let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                // Create a card and add it to the deck
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        let deck : Deck = Deck { cards };
+
+        return deck;
+    }
+}
+
+fn main() {
+    let deck = Deck::new();
+    println!("Here is your deck: {:#?}", deck);
+}
