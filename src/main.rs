75d3bddbb00ece830aa41cba7ce17509f0b2c2a9
José Naves <josenaves@gmail.com>
2026-08-04 14:01:22 -0300

    feat: crates + shuffle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::{rng, seq::SliceRandom};
+
 #[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
@@ -23,9 +25,17 @@ impl Deck {
 
         Deck { cards }
     }
+
+    fn shuffle(&mut self) {
+        let mut rng = rng();
+        self.cards.shuffle(&mut rng);
+    }
 }
 
 fn main() {
-    let deck = Deck::new();
+    let mut deck= Deck::new();
+
+    deck.shuffle();
+
     println!("Here is your deck: {:#?}", deck);
 }
